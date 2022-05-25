44b107ad8abc01d10477b0e7944eb354bfd31901
Muturi KImangu <danmuturikimangu@gmail.com>
2022-05-25 17:03:19 +0300

    update-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      'blue': '#0a192f',
+      'headers': '#ccd6f6',
+      'afterName': '#8892b0',
+      'green': '#47b6a4',
+      'textColor': '#8892b0',
+      'textAccent': '#64ffda',
+      'headerAccent': '#8892b0',
+    },
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      'space Mono': ['Space Mono', 'monospace'],
+    },
   },
-  plugins: [],
 }
